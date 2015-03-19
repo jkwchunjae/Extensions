e4784c5d0671d96618f234e9ab01b13c561482c3
jkwchunjae <jkwchunjae@daum.net>
2015-03-19 11:58:39 +0900

    string.WithVar 함수 추가

--- Extensions/StringHelper.cs
+++ Extensions/StringHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,6 +13,39 @@ namespace Extensions
 		public static string With(this string str, params object[] param)
 		{
 			return string.Format(str, param);
+		}
+
+		/// <summary>
+		/// ex) "{a}, {a:000}, {b}".WithVar(new {a, b});
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="str"></param>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		public static string WithVar<T>(this string str, T arg) where T : class
+		{
+			var type = typeof(T);
+			foreach (var member in type.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+			{
+				if (!(member is FieldInfo || member is PropertyInfo))
+					continue;
+				var pattern = @"\{" + member.Name + @"(\:.*?)?\}";
+				var alreadyMatch = new HashSet<string>();
+				foreach (Match m in Regex.Matches(str, pattern))
+				{
+					if (alreadyMatch.Contains(m.Value)) continue; else alreadyMatch.Add(m.Value);
+					string oldValue = m.Value;
+					string newValue = null;
+					string format = "{0" + m.Groups[1].Value + "}";
+					if (member is FieldInfo)
+						newValue = format.With(((FieldInfo)member).GetValue(arg));
+					if (member is PropertyInfo)
+						newValue = format.With(((PropertyInfo)member).GetValue(arg));
+					if (newValue != null)
+						str = str.Replace(oldValue, newValue);
+				}
+			}
+			return str;
 		}
 
 		public static string StringJoin(this IEnumerable<string> strs, string separator)
