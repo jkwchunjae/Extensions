19a2316d32a9a04e2af2394a073a65f03c6e2cef
jkwchunjae <jkwchunjae@daum.net>
2015-03-31 23:58:25 +0900

    ConvertHelper 추가

---
+++ Extensions/ConvertHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+	public static class ConvertHelper
+	{
+		public static int ConvertToInt32(this byte[] bytes, int startIndex = 0)
+		{
+			return BitConverter.ToInt32(bytes, startIndex);
+		}
+
+		public static string ConvertToString(this byte[] bytes, Encoding encoding)
+		{
+			return encoding.GetString(bytes);
+		}
+	}
+}
