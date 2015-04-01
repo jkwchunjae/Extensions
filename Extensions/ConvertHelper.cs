30c5611afa3add13f1b18f97eee0221b97dddb72
jkwchunjae <jkwchunjae@daum.net>
2015-04-01 18:31:54 +0900

    Convert 관련 Method 추가
    하지만 곧 다시 바꿀거라는게 함정

--- Extensions/ConvertHelper.cs
+++ Extensions/ConvertHelper.cs
@@ -13,9 +13,34 @@ namespace Extensions
 			return BitConverter.ToInt32(bytes, startIndex);
 		}
 
-		public static string ConvertToString(this byte[] bytes, Encoding encoding)
+		public static string GetString(this byte[] bytes, Encoding encoding)
 		{
 			return encoding.GetString(bytes);
+		}
+
+		public static string GetString(this byte[] bytes, int index, int count, Encoding encoding)
+		{
+			return encoding.GetString(bytes, index, count);
+		}
+
+		public static string GetStringUTF8(this byte[] bytes)
+		{
+			return bytes.GetString(Encoding.UTF8);
+		}
+
+		public static string GetStringUTF8(this byte[] bytes, int index, int count)
+		{
+			return bytes.GetString(index, count, Encoding.UTF8);
+		}
+
+		public static T ConvertToEnum<T>(this byte[] bytes, Encoding encoding)
+		{
+			return (T)Enum.Parse(typeof(T), bytes.GetString(encoding));
+		}
+
+		public static T ConvertToEnumUTF8<T>(this byte[] bytes)
+		{
+			return bytes.ConvertToEnum<T>(Encoding.UTF8);
 		}
 	}
 }
