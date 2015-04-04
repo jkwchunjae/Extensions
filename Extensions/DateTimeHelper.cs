30e8434a66f770d3e8cc65ee93726a159798f3b2
jkwchunjae <jkwchunjae@daum.net>
2015-04-04 20:11:45 +0900

    string.ToDate

---
+++ Extensions/DateTimeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+	public static class DateTimeHelper
+	{
+		public static DateTime ToDate(this string str)
+		{
+			return new DateTime(str.Substring(0, 4).ToInt(), str.Substring(4, 2).ToInt(), str.Substring(6, 2).ToInt());
+		}
+	}
+}
