f9d9968a80f5065db148be980120e49cb6ecf2b9
jkwchunjae <jkwchunjae@daum.net>
2015-04-08 18:46:14 +0900

    IEnumerable.GetRandom 추가

---
+++ Extensions/IEnumerableHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+	public static class IEnumerableHelper
+	{
+		public static T GetRandom<T>(this IEnumerable<T> source)
+		{
+			return source.ElementAt(StaticRandom.Next(source.Count()));
+		}
+	}
+}
