f103d2f6b2061a53e658aead753e3fcb36472982
jkwchunjae <jkwchunjae@daum.net>
2015-04-13 01:24:15 +0900

    Date 관련 추가

--- Extensions/DateTimeHelper.cs
+++ Extensions/DateTimeHelper.cs
@@ -12,5 +12,47 @@ namespace Extensions
 		{
 			return new DateTime(str.Substring(0, 4).ToInt(), str.Substring(4, 2).ToInt(), str.Substring(6, 2).ToInt());
 		}
+
+		public static DateTime ToDate(this int value)
+		{
+			return new DateTime(value / 10000, (value / 100) % 100, value % 100);
+		}
+
+		public static int ToInt(this DateTime date)
+		{
+			return date.Year * 10000 + date.Month * 100 + date.Day;
+		}
+
+		public static int AddDays(this int date, int days)
+		{
+			return date.ToDate().AddDays(days).ToInt();
+		}
+
+		public static int Year(this int date)
+		{
+			return date / 10000;
+		}
+
+		public static int Month(this int date)
+		{
+			return (date / 100) % 100;
+		}
+
+		public static int Day(this int date)
+		{
+			return date % 100;
+		}
+
+		public static IEnumerable<int> DateRange(this int beginDate, int endDate, int step = 1)
+		{
+			int fromDate = beginDate;
+			int toDate = endDate;
+
+			while ((step >= 0 ? (fromDate <= toDate) : (fromDate >= toDate)))
+			{
+				yield return fromDate;
+				fromDate = fromDate.AddDays(step);
+			}
+		}
 	}
 }
