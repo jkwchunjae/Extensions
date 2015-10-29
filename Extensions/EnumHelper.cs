a23909567b91818899069495814deb94865a40c1
jkwchunjae <jkwchunjae@daum.net>
2015-10-29 14:22:09 +0900

    string.ParseEnum 추가

---
+++ Extensions/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+	public static class EnumHelper
+	{
+		public static T ParseEnum<T>(this string value)
+		{
+			return (T)Enum.Parse(typeof(T), value);
+		}
+	}
+}
