c70359e7ed2beb73ceb906c700762552231be10b
jkwchunjae <jkwchunjae@daum.net>
2016-12-20 01:42:55 +0900

    IEnumerable<T>.RandomShuffle()

--- Extensions/IEnumerableHelper.cs
+++ Extensions/IEnumerableHelper.cs
@@ -12,5 +12,10 @@ namespace Extensions
 		{
 			return source.ElementAt(StaticRandom.Next(source.Count()));
 		}
+
+		public static IEnumerable<T> RandomShuffle<T>(this IEnumerable<T> source)
+		{
+			return source.Select(x => new { Index = StaticRandom.Next(999999999), T = x }).OrderBy(x => x.Index).Select(x => x.T);
+		}
 	}
 }
