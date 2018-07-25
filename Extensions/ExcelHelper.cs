22aca0d0a305dda9649b0f0de711726d73fec13e
jkwchunjae <jkwchunjae@daum.net>
2018-07-26 00:46:48 +0900

    Excel Library

---
+++ Extensions/ExcelHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public class FieldNameAttribute : Attribute
+    {
+        public string FieldName;
+        public FieldNameAttribute(string fieldName)
+        {
+            FieldName = fieldName;
+        }
+    }
+
+    public static class ExcelHelper
+    {
+        public static T Create<T>(Dictionary<string, object> memberValues) where T : new()
+        {
+            var type = typeof(T);
+            var data = new T();
+
+            var allowTypes = new[] { MemberTypes.Field, MemberTypes.Property };
+            var members = type.GetMembers().Where(x => allowTypes.Contains(x.MemberType));
+
+            foreach (var member in members)
+            {
+                var memberInfoType = member.MemberType == MemberTypes.Field ? typeof(FieldInfo) : typeof(PropertyInfo);
+                var memberType = member.MemberType == MemberTypes.Field ? ((FieldInfo)member).FieldType : ((PropertyInfo)member).PropertyType;
+                var fieldName = (member.GetCustomAttribute(typeof(FieldNameAttribute)) as FieldNameAttribute)?.FieldName ?? member.Name;
+
+                var setValueMethod = memberInfoType.GetMethods()
+                    .Where(x => x.Name == "SetValue")
+                    .FirstOrDefault(x => (x.Attributes & MethodAttributes.Abstract) == 0);
+
+                if (setValueMethod == null)
+                    continue;
+
+                if (memberValues.ContainsKey(fieldName))
+                {
+                    if (memberValues[fieldName] != null)
+                        setValueMethod.Invoke(member, new object[] { data, Convert.ChangeType(memberValues[fieldName], memberType) });
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ Extensions/ExcelWorkbook.cs
@@ -0,0 +1,62 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public class ExcelWorkbook : IDisposable
+    {
+        ExcelPackage _excelPackage;
+
+        public ExcelWorkbook(string path)
+        {
+            _excelPackage = new ExcelPackage(new FileInfo(path));
+        }
+
+        public ExcelWorksheet GetWorksheet(string sheetName)
+        {
+            if (_excelPackage.Workbook.Worksheets.Any(x => x.Name == sheetName))
+            {
+                return _excelPackage.Workbook.Worksheets.First(x => x.Name == sheetName);
+            }
+
+            throw new Exception($"Can't find sheet. SheetName: {sheetName}");
+        }
+
+        public List<T> GetTable<T>(string sheetName, int startRowBase1 = 1, int startColumnBase1 = 1) where T : new()
+        {
+            var sheet = GetWorksheet(sheetName);
+
+            // var availableColumnsCount = sheet.UsedRange.Columns.Count + sheet.UsedRange.Column - startColumnBase1;
+            // var availableRowsCount = sheet.UsedRange.Rows.Count + sheet.UsedRange.Column - startRowBase1;
+
+            var titleInfo = Enumerable.Range(startColumnBase1, 100)
+                .TakeWhile(column => (sheet.Cells[startRowBase1, column]).Value != null)
+                .Select(column => new { Column = column, FieldName = (string)(sheet.Cells[startRowBase1, column]).Value })
+                .ToDictionary(x => x.Column, x => x.FieldName);
+
+            return Enumerable.Range(startRowBase1 + 1, 100000)
+                .TakeWhile(row => ((sheet.Cells[row, titleInfo.Min(x => x.Key)]).Value != null))
+                .AsParallel()
+                .Select(row => new
+                {
+                    Row = row,
+                    ValueDic = titleInfo
+                        .Select(x => new { FieldName = x.Value, Value = (sheet.Cells[row, x.Key]).Value })
+                        .ToDictionary(x => x.FieldName, x => x.Value)
+                })
+                .OrderBy(x => x.Row)
+                .Select(x => ExcelHelper.Create<T>(x.ValueDic))
+                .ToList();
+        }
+
+        public void Dispose()
+        {
+            _excelPackage.Dispose();
+        }
+    }
+}
